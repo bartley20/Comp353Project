142bcf4316310a8733d95fdab4849dd1e212c3de
Alec <alecbart@gmail.com>
2022-05-03 15:48:52 -0500

    Added SQL queries and comments for project reqs

--- ProjectFolder/flaskDemo/forms.py
+++ ProjectFolder/flaskDemo/forms.py
@@ -23,7 +23,9 @@ from flaskDemo.models import Category, Customer, Order, Order_Detail, Product,Su
 # regex=regex1 + regex2
 
 
-
+"""
+16. Checks for empty fields using WTForms validation
+"""
 class PurchaseForm(FlaskForm):
     productList = Product.query.add_columns(Product.ProductID,Product.ProductName,Product.Category_id, Product.Price,Product.Amount_left)
     pList = []
@@ -37,25 +39,6 @@ class PurchaseForm(FlaskForm):
     submit = SubmitField("Buy")
     
 
-
-""" class CreateForm(FlaskForm):
-    employeeList = Employee.query.add_columns(Employee.fname, Employee.lname, Employee.ssn)
-    projectList = Project.query.add_columns(Project.pnumber, Project.pname)
-
-    eList = []
-    for row in employeeList:
-        eList.append((row['fname'] + " " + row['lname'] + " " + row['ssn'],row['fname'] + " " + row['lname'] + " " + row['ssn']))
-    pList = []
-    for row in projectList:
-        pList.append((row['pname'] + " " + str(row['pnumber']),row['pname'] + " " + str(row['pnumber'])))
-
-    employees = SelectField('Employee List', choices=eList,coerce=str)
-    projects = SelectField('Project List', choices=pList,coerce=str)
-    hours=IntegerField('Hours', validators=[DataRequired()])
-
-    submit = SubmitField("Assign")
-
-"""
 class RegistrationForm(FlaskForm):
     name = StringField('Full Name',
                         validators=[DataRequired(), Length(min=2, max=20)])                       
@@ -99,50 +82,3 @@ class UpdateAccountForm(FlaskForm):
             user = Customer.query.filter_by(CustomerEmail=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
-"""
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-    
-class DeptUpdateForm(FlaskForm):
-
-#    dnumber=IntegerField('Department Number', validators=[DataRequired()])
-    dnumber = HiddenField("")
-
-    dname=StringField('Department Name:', validators=[DataRequired(),Length(max=15)])
-#  Commented out using a text field, validated with a Regexp.  That also works, but a hassle to enter ssn.
-#    mgr_ssn = StringField("Manager's SSN", validators=[DataRequired(),Regexp('^(?!000|666)[0-8][0-9]{2}(?!00)[0-9]{2}(?!0000)[0-9]{4}$', message="Please enter 9 digits for a social security.")])
-
-#  One of many ways to use SelectField or QuerySelectField.  Lots of issues using those fields!!
-    mgr_ssn = SelectField("Manager's SSN", choices=myChoices)  # myChoices defined at top
-    
-# the regexp works, and even gives an error message
-#    mgr_start=DateField("Manager's Start Date:  yyyy-mm-dd",validators=[Regexp(regex)])
-#    mgr_start = DateField("Manager's Start Date")
-
-#    mgr_start=DateField("Manager's Start Date", format='%Y-%m-%d')
-    mgr_start = DateField("Manager's start date:", format='%Y-%m-%d')  # This is using the html5 date picker (imported)
-    submit = SubmitField('Update this department')
-
-
-# got rid of def validate_dnumber
-
-    def validate_dname(self, dname):    # apparently in the company DB, dname is specified as unique
-         dept = Department.query.filter_by(dname=dname.data).first()
-         if dept and (str(dept.dnumber) != str(self.dnumber.data)):
-             raise ValidationError('That department name is already being used. Please choose a different name.')
-
-
-class DeptForm(DeptUpdateForm):
-
-    dnumber=IntegerField('Department Number', validators=[DataRequired()])
-    submit = SubmitField('Add this department')
-
-    def validate_dnumber(self, dnumber):    #because dnumber is primary key and should be unique
-        dept = Department.query.filter_by(dnumber=dnumber.data).first()
-        if dept:
-            raise ValidationError('That department number is taken. Please choose a different one.')
-
- """
--- ProjectFolder/flaskDemo/routes.py
+++ ProjectFolder/flaskDemo/routes.py
@@ -22,11 +22,14 @@ mysql = MySQL(app)
 @app.route("/")
 @app.route("/home")
 def home():
-    
-    results2 = Category.query.all()
-    #results = Faculty.query.join(Qualified,Faculty.facultyID == Qualified.facultyID) \
-   #          .add_columns(Faculty.facultyID, Faculty.facultyName, Qualified.Datequalified, Qualified.courseID)
+    """
+    8. Simple SQL query
+    """
+    cur = mysql.connection.cursor()
+    cur.execute("""SELECT * FROM CATEGORY""")
+    results2 = cur.fetchall()
     return render_template('join.html', title='Join', joined_m_n=results2)
+
 @app.route("/allProducts")
 def allProducts():
     
@@ -37,7 +40,11 @@ def allProducts():
 
 @app.route("/category/<cID>")
 def category(cID):
-    category = Product.query.filter(Product.Category_id == cID)
+    cur = mysql.connection.cursor()
+    """ 11. COMPOUND CONDITION """
+    """sqlAlchemy equivalent: category = Product.query.filter(Product.Category_id == cID, product.Amount_left > 0 )"""
+    cur.execute("""SELECT * FROM product WHERE product.Category_id = %s AND product.Amount_left > 0""", [cID])
+    category = cur.fetchall()
     return render_template('about.html', title="Category", category=category, now=datetime.utcnow())
 
 @app.route("/product/<pID>")
@@ -45,6 +52,10 @@ def product(pID):
     category = Product.query.filter(Product.ProductID == pID)
     return render_template('viewProduct.html', title="Category", category=category, now=datetime.utcnow())
 
+
+"""
+6. UPDATE of a record
+"""
 @app.route("/edit", methods=['GET', 'POST'])
 @login_required
 def edit():
@@ -61,23 +72,12 @@ def edit():
     
     
     return render_template('editAccount.html', title='edit', form=form)
-@app.route("/create", methods=['GET', 'POST'])
-def create():
-    form = CreateForm()
-    employeeList = Employee.query.add_columns(Employee.fname, Employee.lname, Employee.ssn)
-    projectList = Project.query.add_columns(Project.pnumber)
 
-    if form.validate_on_submit():
-        employeeSSN = form.employees.data.split()
-        projectNumber = form.projects.data.split()
-        assignment = Works_On(essn=int(employeeSSN[2]), pno=int(projectNumber[1]), hours=form.hours.data)
-        db.session.add(assignment)
-        db.session.commit()
-        flash('You successfully added an assignment','success')
-        return redirect(url_for('home'))
-    
-    return render_template('create.html', title="create", form=form)
-
+"""
+5. Create/insert a record
+14. Form to collect user data
+15. Product field is populated from the database
+"""
 @app.route("/purchase", methods=['GET', 'POST'])
 @login_required
 def purchase():
@@ -101,6 +101,9 @@ def purchase():
     
     return render_template('purchase.html', title="create", form=form)
 
+"""
+7. Delete a record
+"""
 @app.route("/delete/<int:OrderID>", methods=['POST'])
 @login_required
 def delete_order(OrderID):
@@ -109,6 +112,8 @@ def delete_order(OrderID):
     db.session.commit()
     flash('The order has been deleted!', 'success')
     return redirect(url_for('account'))
+
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -131,6 +136,9 @@ def login():
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
+        """
+        9. Filter by query
+        """
         user = Customer.query.filter_by(CustomerEmail=form.email.data).first()
         if user and bcrypt.check_password_hash(user.Password, form.password.data):
             login_user(user,remember=form.remember.data)
@@ -165,90 +173,22 @@ def save_picture(form_picture):
 @login_required
 def account():
     #Want to query all the orders of customerID and get the name of the product
-    
-    myOrders = Order.query.add_columns(Order.OrderID, Product.ProductName, Product.Price, Order_Detail.Quantity)\
+    cur = mysql.connection.cursor()
+
+    """myOrders = Order.query.add_columns(Order.OrderID, Product.ProductName, Product.Price, Order_Detail.Quantity)\
         .filter(Order.CustomerID == current_user.id).\
         join(Order_Detail, Order.OrderID == Order_Detail.OrderID).\
-            join(Product, Product.ProductID == Order_Detail.ProductID)
+            join(Product, Product.ProductID == Order_Detail.ProductID)"""
     
-    user = str(current_user.id)
-    cur = mysql.connection.cursor()
+    """
+    12. JOIN QUERY USING SQL and 13. SUBQUERY USING SQL
+    """
+    cur.execute("""SELECT Orders.OrderID, Product.ProductName, Product.Price, order_detail.Quantity FROM ((Orders INNER JOIN order_detail ON Orders.OrderID = order_detail.OrderID) INNER JOIN Product ON order_detail.ProductID = Product.ProductID) WHERE Orders.CustomerID IN (SELECT id FROM Customer WHERE id = %s )""", [current_user.id])
+    myOrders = cur.fetchall()
+    
+    """ 10. AGGREGATE FUNCTION """
     cur.execute("""SELECT COUNT(orders.OrderID) FROM orders WHERE orders.CustomerID = %s""", [current_user.id])
     ordertotal = cur.fetchall()
     ordertotal = str(*ordertotal[0])
                 
     return render_template('myOrders.html', title='myOrders', orders = myOrders, totalOrders= ordertotal)
-
-
-@app.route("/dept/new", methods=['GET', 'POST'])
-@login_required
-def new_dept():
-    form = DeptForm()
-    if form.validate_on_submit():
-        dept = Department(dname=form.dname.data, dnumber=form.dnumber.data,mgr_ssn=form.mgr_ssn.data,mgr_start=form.mgr_start.data)
-        db.session.add(dept)
-        db.session.commit()
-        flash('You have added a new department!', 'success')
-        return redirect(url_for('home'))
-    return render_template('create_dept.html', title='New Department',
-                           form=form, legend='New Department')
-
-
-@app.route("/dept/<dnumber>")
-@login_required
-def dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    return render_template('dept.html', title=dept.dname, dept=dept, now=datetime.utcnow())
-
-
-@app.route("/assign/<pno>/<essn>")
-def assign(pno,essn):
-    assign = Works_On.query.get_or_404([essn,pno])
-    return render_template('assign.html', title=str(assign.essn) + "_" + str(assign.pno), assign=assign, now=datetime.utcnow())
-
-
-
-
-@app.route("/assign/<essn>/<pno>/delete", methods=['POST'])
-def delete_assign(essn,pno):
-    assign = Works_On.query.get_or_404([essn,pno])
-    db.session.delete(assign)
-    db.session.commit()
-    flash('The assignment has been deleted!', 'success')
-    return redirect(url_for('home'))
-
-@app.route("/dept/<dnumber>/update", methods=['GET', 'POST'])
-@login_required
-def update_dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    currentDept = dept.dname
-
-    form = DeptUpdateForm()
-    if form.validate_on_submit():          # notice we are are not passing the dnumber from the form
-        if currentDept !=form.dname.data:
-            dept.dname=form.dname.data
-        dept.mgr_ssn=form.mgr_ssn.data
-        dept.mgr_start=form.mgr_start.data
-        db.session.commit()
-        flash('Your department has been updated!', 'success')
-        return redirect(url_for('dept', dnumber=dnumber))
-    elif request.method == 'GET':              # notice we are not passing the dnumber to the form
-
-        form.dnumber.data = dept.dnumber
-        form.dname.data = dept.dname
-        form.mgr_ssn.data = dept.mgr_ssn
-        form.mgr_start.data = dept.mgr_start
-    return render_template('create_dept.html', title='Update Department',
-                           form=form, legend='Update Department')
-
-
-
-
-@app.route("/dept/<dnumber>/delete", methods=['POST'])
-@login_required
-def delete_dept(dnumber):
-    dept = Department.query.get_or_404(dnumber)
-    db.session.delete(dept)
-    db.session.commit()
-    flash('The department has been deleted!', 'success')
-    return redirect(url_for('home'))
